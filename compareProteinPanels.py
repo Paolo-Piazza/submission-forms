09ecd45f242f2a9776eddf19340a79047910d495
Paolo-Piazza <101341765+Paolo-Piazza@users.noreply.github.com>
2025-01-31 11:00:24 +0000

    Update compareProteinPanels.py

--- compareProteinPanels.py
+++ compareProteinPanels.py
@@ -16,7 +16,8 @@ def convert_df_to_csv(df):
 
 st.title("CSV List Comparator")
 
-DATA_DIR = "https://github.com/Paolo-Piazza/submission-forms/tree/main"  # Folder where preloaded files are stored
+#DATA_DIR = "https://github.com/Paolo-Piazza/submission-forms/tree/main"  # Folder where preloaded files are stored
+DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 preloaded_files = [f for f in os.listdir(DATA_DIR) if f.endswith(".csv")]
 
 # Checkbox selection for multiple preloaded files
